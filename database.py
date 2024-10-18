83a02c32d6a30bb3c8e3aa4fd68361a135468ef9
Ogbonna Lotachukwu <102598157+lota02@users.noreply.github.com>
2024-10-18 12:29:21 -0700

    Update database.py
    Added comments

--- database.py
+++ database.py
@@ -4,13 +4,14 @@ from datetime import datetime
 def connect_to_database():
     return sqlite3.connect('predictions.db')
 
+# Create a table to store the prdictions 
 def create_table(conn):
     c = conn.cursor()
     c.execute('''DROP TABLE IF EXISTS predictions''')
     c.execute('''CREATE TABLE predictions
                  (student_name TEXT, predicted_result REAL, gpa REAL,timestamp TEXT)''')
     conn.commit()
-
+# Insert the predictions to the table 
 def insert_prediction(conn, student_name, predicted_result, gpa):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     c = conn.cursor()
